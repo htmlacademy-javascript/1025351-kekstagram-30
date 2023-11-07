01e994321217623c6259255e4471cfd5aa602fdb
Danil Razumnyi <razumnyyd@gmail.com>
2023-11-08 00:19:50 +1000

    feat: implement function for creating thumbnails

---
+++ js/thumbnails.js
@@ -0,0 +1,11 @@
+const thumbnailTemplate = document.querySelector('#picture').content.querySelector('.picture');
+
+const createThumbnail = (picturesData) => picturesData.map(({url, description, likes, comments}) => {
+  const thumbnail = thumbnailTemplate.cloneNode(true);
+  thumbnail.querySelector('.picture__img').src = url;
+  thumbnail.querySelector('.picture__img').alt = description;
+  thumbnail.querySelector('.picture__comments').textContent = comments.length;
+  thumbnail.querySelector('.picture__likes').textContent = likes;
+  return thumbnail;
+});
+
