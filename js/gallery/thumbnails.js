5a90f2f941f7c13c7920cc1dc2760f26ae5252c2
Danil Razumnyi <razumnyyd@gmail.com>
2023-11-21 02:27:52 +1000

    feat rendering popup

--- js/gallery/thumbnails.js
+++ js/gallery/thumbnails.js
@@ -1,12 +1,19 @@
 const thumbnailTemplate = document.querySelector('#picture').content.querySelector('.picture');
 const thumbnailsContainer = document.querySelector('.pictures');
 
+const createThumbnailClickHandler = (properties) => (evt) => {
+  evt.preventDefault();
+  document.dispatchEvent(new CustomEvent('thumbnailSelect', {detail: properties}));
+};
+
 const createThumbnails = (picturesData) => picturesData.map(({url, description, likes, comments}) => {
   const thumbnail = thumbnailTemplate.cloneNode(true);
   thumbnail.querySelector('.picture__img').src = url;
   thumbnail.querySelector('.picture__img').alt = description;
   thumbnail.querySelector('.picture__comments').textContent = comments.length;
   thumbnail.querySelector('.picture__likes').textContent = likes;
+  thumbnail.addEventListener('click', createThumbnailClickHandler({url, description, likes, comments}));
+
   return thumbnail;
 });
 
