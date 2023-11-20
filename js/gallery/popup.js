5c269c8d3c3612859ab5b5efb818bcf283be522d
Danil Razumnyi <razumnyyd@gmail.com>
2023-11-20 23:55:12 +1000

    implement rendering comments

--- js/gallery/popup.js
+++ js/gallery/popup.js
@@ -1,9 +1,12 @@
+import { renderComments } from './comments';
+
 const popup = document.querySelector('.big-picture');
 
-const renderPopup = ({url, likes, description}) => {
+const renderPopup = ({url, likes, description, comments}) => {
   popup.querySelector('.big-picture__img').src = url;
   popup.querySelector('.social__caption').textContent = description;
   popup.querySelector('.likes-count').textContent = likes;
+  renderComments(comments);
   popup.classList.remove('hidden');
   document.body.classList.add('.modal-open');
 };
